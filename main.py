053a28af495d1adcb7e3386512658298d366660f
Pratham Agarwal <143127428+Pratham-815@users.noreply.github.com>
2024-02-18 14:00:04 +0000

    Create a function to calculate score

--- main.py
+++ main.py
@@ -10,5 +10,16 @@ def deal_cards():
     return random.choice(card)
 
 
+def calculate_score(cards):
+    """Takes the list of cards and returns the score"""
+
+    if sum(cards)==21 and len(cards)==2:
+        return 0
+    
+    if 11 in cards and sum(cards)>21:
+        cards.remove(11)
+        cards.append(1)
+    return sum(cards)
+
 
     
