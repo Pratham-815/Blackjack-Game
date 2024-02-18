9e728b2595bb527ce64688ad4284e6bf630dedb0
Pratham Agarwal <143127428+Pratham-815@users.noreply.github.com>
2024-02-18 13:59:32 +0000

    Create a function to deal with the cards

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ import sys
 import os
 
 
+def deal_cards():
+    """Returns a random card from the deck"""
+
+    card = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    return random.choice(card)
+
 
 
     
